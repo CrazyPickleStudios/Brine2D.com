c94d68847e97e38ada746e35720cdd9e72d25b15
RichardPreziosi <17318983+RichardPreziosi@users.noreply.github.com>
2026-05-27 15:39:09 -0400

    fixing cod issues.

--- theme/assets/javascripts/code-lang.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll("pre code").forEach(code => {
-        const pre = code.parentElement;
-        const lang = code.className.replace("language-", "") || "text";
-        pre.setAttribute("data-lang", lang);
-    });
-});
