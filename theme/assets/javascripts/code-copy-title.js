762e63af6441364227dca6f8ae5735e10295e2b7
RichardPreziosi <17318983+RichardPreziosi@users.noreply.github.com>
2026-05-27 16:20:27 -0400

    trying to move copy code button to the 'header'.

--- theme/assets/javascripts/code-copy-title.js
+++ theme/assets/javascripts/code-copy-title.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".highlight").forEach(block => {
-        const title = block.querySelector(".md-code__title");
+        const title = block.querySelector("span.filename");
         const nav = block.querySelector(".md-code__nav");
         if (title && nav) {
             title.appendChild(nav);
